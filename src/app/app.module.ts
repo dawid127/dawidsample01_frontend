e1a4ca523ff845d0a37d37b123465ba7340292a3
RAFi <rbogusz@polish-airports.com>
2022-11-30 19:15:08 +0100

    fix button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {ButtonModule} from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WhereyoufromComponent } from './whereyoufrom/whereyoufrom.component';
 import { WhatareyoudoingforlifeComponent } from './whatareyoudoingforlife/whatareyoudoingforlife.component';
+import {ButtonModule} from 'primeng/button';
 
 
 @NgModule({
@@ -17,7 +17,7 @@ import { WhatareyoudoingforlifeComponent } from './whatareyoudoingforlife/whatar
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
